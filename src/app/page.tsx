1fa9faa3f4cff75f38a2aed0c362f2f46a53d324
bilbini <said@welinxya.com>
2025-12-24 00:59:07 +0100

    Auto-sync after agent response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ import dynamic from "next/dynamic";
 
 const Hero = dynamic(() => import("@/components/home/Hero").then((mod) => mod.Hero), { ssr: false });
 const LogoStrip = dynamic(() => import("@/components/home/LogoStrip").then((mod) => mod.LogoStrip), { ssr: false });
+const ServicesOverview = dynamic(() => import("@/components/home/ServicesOverview").then((mod) => mod.ServicesOverview), { ssr: false });
+const Benefits = dynamic(() => import("@/components/home/Benefits").then((mod) => mod.Benefits), { ssr: false });
+const Process = dynamic(() => import("@/components/home/Process").then((mod) => mod.Process), { ssr: false });
+const Stats = dynamic(() => import("@/components/home/Stats").then((mod) => mod.Stats), { ssr: false });
+const TechStack = dynamic(() => import("@/components/home/TechStack").then((mod) => mod.TechStack), { ssr: false });
+const FeaturedCaseStudy = dynamic(() => import("@/components/home/FeaturedCaseStudy").then((mod) => mod.FeaturedCaseStudy), { ssr: false });
+const Testimonials = dynamic(() => import("@/components/home/Testimonials").then((mod) => mod.Testimonials), { ssr: false });
+const FAQ = dynamic(() => import("@/components/home/FAQ").then((mod) => mod.FAQ), { ssr: false });
+const CTAStrip = dynamic(() => import("@/components/home/CTAStrip").then((mod) => mod.CTAStrip), { ssr: false });
 
 export default function Home() {
   const [mounted, setMounted] = useState(false);
@@ -21,6 +30,15 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Hero />
       <LogoStrip />
+      <ServicesOverview />
+      <Benefits />
+      <Process />
+      <Stats />
+      <TechStack />
+      <FeaturedCaseStudy />
+      <Testimonials />
+      <FAQ />
+      <CTAStrip />
     </div>
   );
 }
